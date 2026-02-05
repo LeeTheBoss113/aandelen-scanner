a8101b510c8c20e4b0ff0a9323113abb940fa1f5
LeeTheBoss113 <leonvandenbos@gmail.com>
2026-02-05 16:13:09 +0100

    Update DT.py

--- DT.py
+++ DT.py
@@ -12,8 +12,12 @@ def ld():
  try: return pd.read_csv(F).to_dict('records')
  except: return []
 
-def sv(d): pd.DataFrame(d).to_csv(F, index=False)
-if 'pf' not in st.session_state: st.session_state.pf = ld()
+def sv(d):
+ df = pd.DataFrame(d)
+ df.to_csv(F, index=False)
+
+if 'pf' not in st.session_state:
+ st.session_state.pf = ld()
 
 with st.sidebar:
  st.header("Beheer")
@@ -24,10 +28,13 @@ with st.sidebar:
   if st.form_submit_button("OK"):
    if t:
     st.session_state.pf.append({"T":t,"I":i,"P":p})
-    sv(st.session_state.pf); st.rerun()
+    sv(st.session_state.pf)
+    st.rerun()
  for n, m in enumerate(st.session_state.pf):
   if st.sidebar.button(f"X {m['T']}", key=f"d{n}"):
-   st.session_state.pf.pop(n); sv(st.session_state.pf); st.rerun()
+   st.session_state.pf.pop(n)
+   sv(st.session_state.pf)
+   st.rerun()
 
 ML = ['KO','PEP','JNJ','O','PG','ABBV','CVX','XOM','MMM','T','VZ','WMT','LOW','TGT','ABT','MCD','MSFT','AAPL','IBM','HD','COST','LLY','PFE','MRK','UNH','BMY','SBUX','CAT','DE','NEE','PM','MO','BLK','V','MA','AVGO','TXN','JPM','SCHW']
 AL = list(set(ML + [x['T'] for x in st.session_state.pf]))
@@ -35,7 +42,8 @@ AL = list(set(ML + [x['T'] for x in st.session_state.pf]))
 @st.cache_data(ttl=3600)
 def gd(s):
  try:
-  tk = yf.Ticker(s); h = tk.history(period="2y")
+  tk = yf.Ticker(s)
+  h = tk.history(period="2y")
   return {"h":h,"i":tk.info,"p":h['Close'].iloc[-1]}
  except: return None
 
@@ -46,32 +54,13 @@ for t in AL:
  p, h, inf = d['p'], d['h'], d['i']
  r = round(ta.rsi(h['Close'], 14).iloc[-1], 1)
  m = h['Close'].tail(200).mean()
- p6 = round(((p-h['Close'].iloc[-126])/h['Close'].iloc[-126])*100,1)
- p1 = round(((p-h['Close'].iloc[-252])/h['Close'].iloc[-252])*100,1)
+ c = h['Close']
+ p6 = round(((p-c.iloc[-126])/c.iloc[-126])*100,1)
+ p1 = round(((p-c.iloc[-252])/c.iloc[-252])*100,1)
  s = "WAIT"
  if p > m:
   s = "OK"
   if r < 42: s = "BUY"
   if r > 75: s = "HIGH"
  a = "HOLD"
- if p < m: a = "SELL"
- elif r > 75: a = "TAKE"
- for pi in st.session_state.pf:
-  if pi['T'] == t:
-   w = (pi['I']/pi['P'])*p
-   pr.append({"T":t,"P":p,"W$":round(w-pi['I'],2),"W%":round(((w-pi['I'])/pi['I'])*100,1),"6M":p6,"1Y":p1,"Stat":s,"Adv":a})
- if t in ML:
-  sr.append({"T":t,"P":p,"D%":round((inf.get('dividendYield',0) or 0)*100,2),"Stat":s,"RSI":r})
-
-st.title("Stability Investor")
-t1, t2 = st.tabs(["Portfolio", "Scanner"])
-
-def stl(df):
- c = {"BUY":"color:green","SELL":"color:red","TAKE":"color:orange"}
- try: return df.style.apply(lambda x: [c.get(v, "") for v in x], axis=0)
- except: return df
-
-with t1:
- if pr:
-  dfp = pd.DataFrame(pr)
-  st.metric("Profit", round(dfp['W$'].sum
+ if p < m: a =
